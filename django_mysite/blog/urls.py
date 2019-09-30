98985218a792d31683496244a680ecd2e2890298
LJM760993108 <760993108@qq.com>
2019-09-30 09:36:04 +0800

    use template and user management

--- django_mysite/blog/urls.py
+++ django_mysite/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.blog_title),
+    path("", views.blog_title),# "" 表示访问根目录，在blog 中代表 根目录/blog/
+    path("<int:article_id>/", views.blog_article),
+
 ]
--- django_mysite/blog/views.py
+++ django_mysite/blog/views.py
@@ -5,4 +5,9 @@ def blog_title(request):
     blogs = BlogArticles.objects.all()
     return render(request, 'blog/titles.html', {"blogs":blogs})
 
+def blog_article(request, article_id):
+    article = BlogArticles.objects.get(id=article_id)
+    pub = BlogArticles.publish
+    return render(request, "blog/content.html",{"article":article, "publish":pub})
+
 # Create your views here.
